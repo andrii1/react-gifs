b28cc8112fe679bcd1d5fee8220502c18e9f3fa3
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2022-03-02 11:51:20 +0100

    update

---
+++ .history/src/components/App_20220302114649.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import giphy from 'giphy-api';
+import Search from './Search'
+import Show from './Show'
+import Sidebar from './Sidebar'
+const GIPHY_API_KEY = '1KMPHCBIOe3hOjJwCJQX49sRc6cM0oIm';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "xT9IgDEI1iZyb2wqo8"
+    };
+    this.search = this.search.bind(this);
+    //this.selectGif = this.selectGif.bind(this);
+  }
+
+  search(query) {
+    const giphEndpoint = `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${query}&limit=10`
+    fetch(giphEndpoint).then(response => response.json()).then((data) => {
+      const gifs = data.data.map(giph => {id: giph.id})
+      this.setState({
+        gifs: gifs
+      })
+      console.log(gifs)
+    })
+  }
+
+
+  render() {
+    return (<div className="app">
+
+        <div className="left">
+          <div className="left-container">
+          <Search searchFunction={this.search} />
+          <Show id={this.state.selectedGifId}  />
+          </div>
+        </div>
+        <div className="right"><Sidebar gifs={this.state.gifs} /></div>
+
+    </div>);
+  }
+}
+
+export default App;
